fd80ceaa3f14d57b9d4f08ea247f277c0d1f9356
Jozef Valko <valko14@student.fiit.stuba.sk>
2016-04-12 18:01:07 +0200

    import Volley library

---
+++ app/src/main/java/com/example/jozef/predajelektroniky/data/GetAll.java
@@ -0,0 +1,13 @@
+package com.example.jozef.predajelektroniky.data;
+
+import com.android.volley.RequestQueue;
+
+/**
+ * Created by Jozef on 12.04.2016.
+ */
+public class GetAll {
+    public RequestQueue mRequestQueue;
+
+
+
+}
